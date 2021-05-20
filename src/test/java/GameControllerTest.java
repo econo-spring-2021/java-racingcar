41080aa3217afc104addfc48afa908608e1ea632
Hongdonggeon <ghd700@naver.com>
2021-05-20 11:18:40 +0900

    test code naming refactoring

--- src/test/java/GameControllerTest.java
+++ src/test/java/GameControllerTest.java
@@ -1,28 +1,28 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.controller.GameController;
+import racingcar.view.InputView;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameControllerTest {
-    @DisplayName("차 번호 갯수 테스트")
+    @DisplayName("자동차 이름 쉼표 구분 테스트")
     @Test
-    void separateName()  {
+    void separateNameTest()  {
         //given
-        final GameController game = new GameController();
         String names = "kim,park,hong";
         String[] cars = {"kim","park","hong"};
         //when
-        String[] namesSeparate = game.separateName(names);
+        String[] namesSeparate = InputView.separateName(names);
         //then
         assertArrayEquals(cars,namesSeparate);
     }
 
 
-    @DisplayName("최소 참가 인원 테스트")
+    @DisplayName("최소 인원(2명) 제한 테스트 ")
     @Test
-    void checkIncludeCarNum() {
+    void checkIncludeCarNumTest() {
         //given
         final GameController game = new GameController();
         String[] cars = {"mike"};
@@ -34,7 +34,7 @@ class GameControllerTest {
 
     @DisplayName("랜덤 값 범위 테스트")
     @Test
-    void randomValueGive() {
+    void randomValueGiveTest() {
         //given
         final GameController game = new GameController();
         //when
@@ -43,25 +43,13 @@ class GameControllerTest {
         assertTrue(random>=1 && random <=9);
     }
 
-    @DisplayName("차 이름 글자 제한 초과 테스트")
+    @DisplayName("차 이름 길이(5글자) 초과 제한 테스트")
     @Test
-    void InningFiveLength(){
+    void InningFiveLengthTest(){
         //given
         final GameController game = new GameController();
         //when
         boolean result = game.InningFiveLength("asdfgg");
-        //then
-        assertFalse(result);
-    }
-
-    @DisplayName("최소 참가 인원 이상 여부 테스트")
-    @Test
-    void checkIncludeNum(){
-        //given
-        final GameController game = new GameController();
-        String[] participations ={"mike"};
-        //when
-        boolean result = game.checkIncludeCarNum(participations);
         //then
         assertFalse(result);
     }
